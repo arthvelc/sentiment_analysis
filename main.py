c9e0fc21bc46994051126bd01760d011f0a1f6cf
Arturo Solares <arturosolaresps@gmail.com>
2023-04-08 15:33:14 -0600

    proyecto de análisis terminado, con cuestiones que se pueden mejorar tanto en el código fuente como en los archivos que acompaña el repositorio, tales como el requirements.txt y el README.md

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 # Análisis de sentimientos
 import numpy as np
 
-def generate_list_of_words(strings): #trasforma el tweet en una lista de palabras
-    strings = strings.replace("!", "").replace(",", "").replace(".", ""). replace("()", "").replace(")", "").lower().split()
+
+# trasforma el tweet en una lista de palabras
+def generate_list_of_words(strings):
+    strings = strings.replace("!", "").replace(",", "").replace(
+        ".", ""). replace("()", "").replace(")", "").lower().split()
     return strings
 
-def generate_vector_w(words_list, tweet_list): #w_vector es el número de elementos que existen en el tweet de una arrat ya definido
+
+# w_vector es el número de elementos que existen en el tweet de una arrat ya definido
+def generate_vector_w(words_list, tweet_list):
     pass
     w_vector = np.zeros(len(words_list))
-    
+
     for element in range(len(words_list)):
         if words_list[element] in tweet_list:
-            w_vector[element]+= 1
-    
+            w_vector[element] += 1
+
     return w_vector
-            
-def generate_vector_s(tweet_list): # s_vector es nuestro vector de sentimientos contabilizando el número de palabras positivas, negativas y neutras de nuestra lista de palabras
+
+
+# s_vector es nuestro vector de sentimientos contabilizando el número de palabras positivas, negativas y neutras de nuestra lista de palabras
+def generate_vector_s(tweet_list):
     positive = ['excelente', 'gran', 'positivo']
     neutral = ['pérdida']
     negative = ['muerte', 'luto']
     s_vector = np.zeros(3)
-    
+
     for i in range(len(tweet_list)):
         if tweet_list[i] in positive:
             s_vector[0] += 1
@@ -30,11 +37,72 @@ def generate_vector_s(tweet_list): # s_vector es nuestro vector de sentimientos 
             s_vector[1] += 1
 
     return s_vector
-    
+
+# media de cada elemento del vector
 def mean_w_vector(w_vector):
     return round(np.mean(w_vector), 2)
 
+# score del vector s
+def generate_score(s_vector):
+    score_vector = np.array((1, 0, -1))
+    return np.dot(score_vector, s_vector)
+
+# análisis por cada tweet
+def sentiment_analysis_results(tweet_1, tweet_2, tweet_3, tweet_4):
+    words_to_find = "muerte, pérdida, luto, excelente, gran y positivo"
+    words_to_find = generate_list_of_words(words_to_find)
+
+    # tweet_1 results
+    tweet_1 = generate_list_of_words(tweet_1)
+    w_vector_tweet_1 = generate_vector_w(words_to_find, tweet_1)
+    s_vector_tweet_1 = generate_vector_s(tweet_1)
+    w_vector_mean_tweet_1 = mean_w_vector(w_vector_tweet_1)
+    s_vector_score_tweet_1 = generate_score(s_vector_tweet_1)
+    # tweet_2 results
+    tweet_2 = generate_list_of_words(tweet_2)
+    w_vector_tweet_2 = generate_vector_w(words_to_find, tweet_2)
+    s_vector_tweet_2 = generate_vector_s(tweet_2)
+    w_vector_mean_tweet_2 = mean_w_vector(w_vector_tweet_2)
+    s_vector_score_tweet_2 = generate_score(s_vector_tweet_2)
+    # tweet 3 results
+    tweet_3 = generate_list_of_words(tweet_3)
+    w_vector_tweet_3 = generate_vector_w(words_to_find, tweet_3)
+    s_vector_tweet_3 = generate_vector_s(tweet_3)
+    w_vector_mean_tweet_3 = mean_w_vector(w_vector_tweet_3)
+    s_vector_score_tweet_3 = generate_score(s_vector_tweet_3)
+    # tweet_4 results
+    tweet_4 = generate_list_of_words(tweet_4)
+    w_vector_tweet_4 = generate_vector_w(words_to_find, tweet_4)
+    s_vector_tweet_4 = generate_vector_s(tweet_4)
+    w_vector_mean_tweet_4 = mean_w_vector(w_vector_tweet_4)
+    s_vector_score_tweet_4 = generate_score(s_vector_tweet_4)
+
+    print(f""" 
+    Los resultados de tweet 1 fueron:
+    w = {w_vector_tweet_1}
+    s = {s_vector_tweet_1}
+    media del vector w = {w_vector_mean_tweet_1}
+    producto punto o score = {s_vector_score_tweet_1}
+
+    Los resultados de tweet 2 fueron: 
+    w = {w_vector_tweet_2}
+    s = {s_vector_tweet_2}
+    media del vector w = {w_vector_mean_tweet_2}
+    producto punto o score = {s_vector_score_tweet_2}
+
+    Los resultados de tweet 3 fueron: 
+    w = {w_vector_tweet_3}
+    s = {s_vector_tweet_3}
+    media del vector w = {w_vector_mean_tweet_3}
+    producto punto o score = {s_vector_score_tweet_3}
     
+    Los resultados de tweet 4 fueron: 
+    w = {w_vector_tweet_4}
+    s = {s_vector_tweet_4}
+    media del vector w = {w_vector_mean_tweet_4}
+    producto punto o score = {s_vector_score_tweet_4}""")
+
+
 if __name__ == '__main__':
 
     tweet_1 = "Gran mexicano y excelente en su área, su muerte es una enorme pérdida y debería ser luto nacional!!!"
@@ -45,17 +113,4 @@ if __name__ == '__main__':
 
     tweet_4 = "Soy docente universitario, estoy intentando preparar mis clases en modo platzi bien didáctico, (le llamo modo noticiero), descargue una plataforma gratuita de grabación y transmisión de vídeo, se llama Obs estudio!bueno la sigo remando con sus funciones pero sé que saldrá algo!"
 
-    words_to_find = "muerte, pérdida, luto, excelente, gran y positivo"
-    words_to_find = generate_list_of_words(words_to_find)
-    
-    tweet_1 = generate_list_of_words(tweet_1) 
-    
-    w_vector_tweet_1 = generate_vector_w(words_to_find, tweet_1) 
-    
-    s_vector_tweet_1 = generate_vector_s(tweet_1) 
-
-    w_vector_mean_tweet_1 = mean_w_vector(w_vector_tweet_1)
-    
-
-
-
+    result = sentiment_analysis_results(tweet_1, tweet_2, tweet_3, tweet_4)
